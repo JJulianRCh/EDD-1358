be172ce66efbc69a6c627346a4582e4968abfea8
JJulianRCh <111546604+JJulianRCh@users.noreply.github.com>
2022-08-22 23:09:24 -0500

    Update RedesSociales.py
    Se quito el modulo "Locale" debido a una modificación del archivo "presenciaredes.csv" que se quito las comillas y comas en valores numéricos del archivo que ya no es necesario una localización para estos.

--- RedesSociales.py
+++ RedesSociales.py
@@ -10,19 +10,18 @@ Hacer un programa que haga lo siguiente:
 - Calcular el promedio de crecimiento de Twitter y Facebook entre los meses de enero a junio.
 """
 
-import locale
 import csv
 
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')  # Ajuste de localizacion para los numeros
+lista = []
 try:
-  archivoCSV = open('presenciaredes.csv', encoding='UTF-8')  # Cargamos el archivo csv en una variable
-  lista = list(csv.reader(archivoCSV))  # Lo guardamos en una lista
+    archivoCSV = open('presenciaredes.csv', encoding='UTF-8')  # Cargamos el archivo csv en una variable
+    lista = list(csv.reader(archivoCSV))  # Lo guardamos en una lista
 except FileNotFoundError:
-  print("Archivo no encontrado")
+    print("Archivo no encontrado")
 
 # Mostrar la diferencia de seguidores en Twitter entre el mes de Enero y Junio
 # Restamos la cantidad de seguidores en Junio con el de Enero para obtener la diferencia
-diferencia = int(locale.atof(lista[8][8])) - int(locale.atof(lista[8][3]))
+diferencia = int(lista[8][8]) - int(lista[8][3])
 print("La diferencia de seguidores en Twitter entre Enero y Junio es de", diferencia, "seguidores")
 
 # Calcular la diferencia de visualizaciones de YouTube
@@ -33,9 +32,9 @@ diferencia = 0
 if meses.__contains__(mes1.upper()) and meses.__contains__(mes2.upper()):
     for columna in range(3, 9):
         if lista[0][columna] == mes1.upper():
-            diferencia += int(locale.atof(lista[16][columna]))
+            diferencia += int(lista[16][columna])
         if lista[0][columna] == mes2.upper():
-            diferencia -= int(locale.atof((lista[16][columna])))
+            diferencia -= int(lista[16][columna])
     print("La diferencia de visualizacion de Youtube entre " + mes1 + " y " + mes2 + " es de", abs(diferencia))
 else:
     print("Ingreso de dato(s) no valido")
@@ -44,8 +43,8 @@ else:
 promedioFace = 0
 promedioTwitter = 0
 for columna in range(3, 9):
-    promedioFace += float(locale.atof(lista[2][columna]))
-    promedioTwitter += float(locale.atof((lista[9][columna])))
+    promedioFace += float(lista[2][columna])
+    promedioTwitter += float(lista[9][columna])
 promedioFace /= 6
 promedioTwitter /= 6
 print("El promedio de crecimiento de Facebook es de {:.1f}".format(promedioFace))
