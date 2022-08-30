3c81386996cd8839de5d364b77e162cedb3e7642
JJulianRCh <111546604+JJulianRCh@users.noreply.github.com>
2022-08-29 22:44:21 -0500

    Update testArregloADT.java

--- Tarea 2/testArregloADT.java
+++ Tarea 2/testArregloADT.java
@@ -4,7 +4,6 @@ public class testArregloADT {
 
     public static void main(String[] args) {
         ArregloADT lista = new ArregloADT(5);
-        Object[] valor = new Object[1];
         lista.setElemento(0, "Hola");
         lista.setElemento(3, new Persona("Julian", 20, 1.76f));
         lista.setElemento(1, 3);
@@ -13,7 +12,7 @@ public class testArregloADT {
             System.out.println("[" + i + "] = {" + lista.getElemento(i) + "}");
         }
         System.out.println(lista);
-        lista.limpiar(valor);
+        lista.limpiar("Vacio");
         System.out.println(lista);
     }
 }
